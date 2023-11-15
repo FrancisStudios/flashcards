fdbaa4cbe6da90f34dd7cc0c560344f2aa408479
unknown <vlfmap@gmail.com>
2023-11-15 08:55:28 +0100

    added controls hooks for btns

--- src/menu.gd
+++ src/menu.gd
@@ -1,11 +1,25 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
+	pass
+
+func _on_dict_config_pressed():
 	pass # Replace with function body.
 
+func _on_about_pressed():
+	pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_learn_pressed():
+	pass # Replace with function body.
+
+func _on_freeplay_pressed():
+	pass # Replace with function body.
+	
+func _on_20_pressed():
+	pass # Replace with function body.
+
+func _on_50_pressed():
+	pass # Replace with function body.
+
+func _on_100_pressed():
+	pass # Replace with function body.
