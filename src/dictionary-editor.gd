2c8185d83e8eaf00aa330c049a86295c2f779fd5
unknown <vlfmap@gmail.com>
2023-11-16 08:48:49 +0100

    item list wired in

--- src/dictionary-editor.gd
+++ src/dictionary-editor.gd
@@ -1,4 +1,12 @@
 extends Control
+
+func _ready():
+	var itemList = get_node('NinePatchRect/ItemList')
+	print(global.DICTIONARY)
+	for itemIndex in global.DICTIONARY.size():
+		var itemText = global.DICTIONARY[itemIndex]['original'] + ' = ' +global.DICTIONARY[itemIndex]['translation']
+		itemList.add_item(itemText, null, true)
+
  
 func _on_back_pressed():
 	get_tree().change_scene_to_file("res://src/scenes/load_screen.tscn")
