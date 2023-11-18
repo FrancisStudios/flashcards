40afa7bbbad5964cf505cbdff6f8f4399fdc26a7
unknown <vlfmap@gmail.com>
2023-11-18 17:10:44 +0100

    added animations

--- src/game.gd
+++ src/game.gd
@@ -1,7 +1,7 @@
 extends Node3D
 
 func _ready():
-	pass 
+	flipUpNextCard()
 
 func _process(delta):
 	pass
@@ -9,3 +9,11 @@ func _process(delta):
 func _on_back_pressed():
 	if global.NEXT_SCENE_INSTRUCTIONS == global.SCENE_INSTRUCTIONS.FREEPLAY:
 		global.init_next_scene("res://home.tscn")
+
+func flipUpNextCard():
+	$QuestionCard/FlipUp.play("flip_question_card")
+	$QuestionCard/FlipUp.connect("animation_started", swipeOutThisCard)
+
+func swipeOutThisCard():
+	print('blin')
+	$QuestionCard/FlipUp.play("swipe_out")
