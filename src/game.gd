769c6d2d1b151695792d8845c56b70040c65f086
unknown <vlfmap@gmail.com>
2023-11-18 19:24:50 +0100

    timer

--- src/game.gd
+++ src/game.gd
@@ -1,19 +1,28 @@
 extends Node3D
 
 func _ready():
-	flipUpNextCard()
+	start_card_procedure()
 
 func _process(delta):
+	print($Timer.time_left)
 	pass
 
 func _on_back_pressed():
 	if global.NEXT_SCENE_INSTRUCTIONS == global.SCENE_INSTRUCTIONS.FREEPLAY:
 		global.init_next_scene("res://home.tscn")
 
+func start_card_procedure():
+	$Timer.start(6.5)
+	flipUpNextCard()
+	pass
+	
 func flipUpNextCard():
 	$QuestionCard/FlipUp.play("flip_question_card")
-	$QuestionCard/FlipUp.connect("animation_started", swipeOutThisCard)
+	$QuestionCard/FlipUp.queue("swipe_out")
+	$QuestionCard/FlipUp.queue("flip_question_card")
+	$QuestionCard/FlipUp.connect("animation_finished", swipeOutThisCard)
 
 func swipeOutThisCard():
 	print('blin')
-	$QuestionCard/FlipUp.play("swipe_out")
+	print($Timer.time_left)
+	$Timer.stop()
