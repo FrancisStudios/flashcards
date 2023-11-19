379e21e1a1ad3fdf3a56c093c8f2b2fd8eed0c68
unknown <vlfmap@gmail.com>
2023-11-19 08:13:09 +0100

    basic time flow

--- src/game.gd
+++ src/game.gd
@@ -1,5 +1,8 @@
 extends Node3D
 
+var GENERATED_WORDS = global.DICTIONARY # only test setup
+var GAME_INDEX:int = 0
+
 func _ready():
 	start_card_procedure()
 	
@@ -13,21 +16,29 @@ func _on_back_pressed():
 		global.init_next_scene("res://home.tscn")
 
 func start_card_procedure():
-	$Timer.wait_time = 10.0
-	$Timer.start()
-	$Timer.timeout.connect(swipeOutThisCard)
-	flipUpNextCard()
+	if GAME_INDEX == 0:
+		flipUpFirstCard()
+	else:
+		flipUpNextCard()
+		
+func flipUpFirstCard():
+	$QuestionCard/QuestionCardLabel.text = GENERATED_WORDS[GAME_INDEX]['original']
+	$QuestionCard/FlipUp.play("flip_question_card")
 	
 func flipUpNextCard():
-	$QuestionCard/FlipUp.play("flip_question_card")
-	$QuestionCard/FlipUp.queue("swipe_out")
-	$QuestionCard/FlipUp.queue("flip_question_card")
-	$QuestionCard/FlipUp.animation_finished.connect(swipeOutThisCard)
+	if GAME_INDEX <= GENERATED_WORDS.size():
+		$QuestionCard/FlipUp.play("flip_question_card")
+		$QuestionCard/FlipUp.queue("swipe_out")
+		$QuestionCard/FlipUp.queue("flip_question_card")
+		# $QuestionCard/FlipUp.animation_finished.connect(swipeOutThisCard)
 
 func swipeOutThisCard():
-	print('blin')
+	print('swipe out can happen')
 	print($Timer.time_left)
 	# $Timer.stop()
 
 func _on_flip_up_animation_finished(anim_name):
-	print('anim finished' + anim_name)
+	if anim_name == "flip_question_card":
+		$Timer.wait_time = 10.0
+		$Timer.start()
+		$Timer.timeout.connect(swipeOutThisCard)
