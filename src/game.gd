d44d0a31903d34ef7efa42eb4dcb772dc6c6a208
unknown <vlfmap@gmail.com>
2023-11-19 14:04:42 +0100

    added correction

--- src/game.gd
+++ src/game.gd
@@ -2,15 +2,19 @@ extends Node3D
 
 var GENERATED_WORDS = global.DICTIONARY # only test setup
 var GAME_INDEX:int = 0
+var TIMER_VALUE: float = 20.0
 
 func _ready():
 	start_card_procedure()
 	
 
 func _process(delta):
-	# print($Timer.time_left)
+	
+	var timePercentage = ($Timer.time_left / (TIMER_VALUE/100))
+	$Camera3D/Control/ProgressBar.value = timePercentage
+	
 	if Input.is_action_just_pressed("submit"):
-		print('a word is submited: ' + $Camera3D/Control/Input.text)
+		wordWasSubmitted($Camera3D/Control/Input.text)
 
 func _on_back_pressed():
 	if global.NEXT_SCENE_INSTRUCTIONS == global.SCENE_INSTRUCTIONS.FREEPLAY:
@@ -32,7 +36,15 @@ func flipUpNextCard():
 		$QuestionCard/FlipUp.queue("swipe_out")
 		$QuestionCard/FlipUp.queue("flip_question_card")
 		# $QuestionCard/FlipUp.animation_finished.connect(swipeOutThisCard)
-
+		
+func wordWasSubmitted(answer: String):
+	
+	if answer == GENERATED_WORDS[GAME_INDEX]['translation']:
+		$Feedback/Success.visible = true
+	else:
+		$Feedback/Fail.visible = true
+		$Feedback/Fail/Correction.text = GENERATED_WORDS[GAME_INDEX]['translation']
+		
 func swipeOutThisCard():
 	print('swipe out can happen')
 	print($Timer.time_left)
@@ -40,6 +52,6 @@ func swipeOutThisCard():
 
 func _on_flip_up_animation_finished(anim_name):
 	if anim_name == "flip_question_card":
-		$Timer.wait_time = 10.0
+		$Timer.wait_time = TIMER_VALUE
 		$Timer.start()
 		$Timer.timeout.connect(swipeOutThisCard)
