194fb36793a3f4095d84ebc6fd27fe2b4f482be2
unknown <vlfmap@gmail.com>
2023-11-20 19:26:31 +0100

    added content to save and about

---
+++ src/about.gd
@@ -0,0 +1,4 @@
+extends Control
+
+func _on_back_pressed():
+	global.init_next_scene("res://home.tscn")
---
+++ src/settings.gd
@@ -0,0 +1,18 @@
+extends Control
+
+
+func _ready():
+	pass
+
+func _process(delta):
+	pass
+
+
+
+# WRAP-UP BUTTONS
+
+func _on_save_pressed():
+	pass # Replace with function body.
+
+func _on_cancel_pressed():
+	global.init_next_scene("res://home.tscn")
